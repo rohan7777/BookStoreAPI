06e0b3a85b84ee1cb8de9425ea4113ee5aacd6a4
Rohan Shahane <shahane.rohan96@gmail.com>
2018-12-31 19:13:44 +0530

    Added logic to check login credentials in DB
    Added logic for validating username and password.
    TODO : Send responce back in appropriate format

---
+++ CRUD/Login.cs
@@ -0,0 +1,39 @@
+﻿using BookStoreAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace BookStoreAPI.CRUD
+{
+    public class Login
+    {
+        public bool handleLogin(LoginModel val)
+        {
+            var con = @"Data Source=IL013449;Initial Catalog=Test;User ID=sa;Password=~$ystem32";
+            using (SqlConnection myConnection = new SqlConnection(con))
+            {
+                SqlCommand command = new SqlCommand("SELECT * FROM LoginTable", myConnection);
+                myConnection.Open();
+
+                SqlDataReader reader = command.ExecuteReader();
+                while (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        if(reader["email"].ToString() == val.email)
+                        {
+                            if(reader["password"].ToString() == val.password)
+                            {
+                                return true;
+                            }
+                        }
+                    }
+                    reader.NextResult();
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ Controllers/LoginController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using BookStoreAPI.Models;
+using BookStoreAPI.CRUD;
+
+namespace BookStoreAPI.Controllers
+{
+    public class LoginController : ApiController
+    {
+        // GET: api/Login
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Login/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Login
+        public string Post(LoginModel val)
+        {
+            Login obj = new Login();
+            if (obj.handleLogin(val))
+            {
+                return "Login Successful!";
+            }
+            else
+            {
+                return "Login Failed.";
+            }
+            
+        }
+
+        // PUT: api/Login/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Login/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
