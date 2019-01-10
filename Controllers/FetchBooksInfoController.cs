fb409cd4df509027f94593616da9a5189bd9ec71
Rohan Shahane <shahane.rohan96@gmail.com>
2019-01-10 17:19:19 +0530

    Created API for fetching data from Books table
    Created an API which would fetch all the data from the Books table in the database and return appropriate JSON responce.

---
+++ Controllers/FetchBooksInfoController.cs
@@ -0,0 +1,45 @@
+﻿using BookStoreAPI.CRUD;
+using BookStoreAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace BookStoreAPI.Controllers
+{
+    public class FetchBooksInfoController : ApiController
+    {
+        // GET: api/FetchBooksInfo
+        //public IEnumerable<string> Get()
+        public List<FetchBooksInfoModel> Get()
+        {
+            FetchBooksInfoLogic obj = new FetchBooksInfoLogic();
+            obj.returnBookInfo();
+            //return new string[] { "value1", "value2" };
+            return obj.returnBookInfo();
+        }
+
+        // GET: api/FetchBooksInfo/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/FetchBooksInfo
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/FetchBooksInfo/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/FetchBooksInfo/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Models/FetchBooksInfoModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookStoreAPI.Models
+{
+    public class FetchBooksInfoModel
+    {
+        // Model for returning all the details of the book in the database
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public string Description { get; set; }
+        public float Price { get; set; }
+        public int Quantity { get; set; }
+        public int CategoryID { get; set; }
+        public string IconURL { get; set; }
+    }
+}
