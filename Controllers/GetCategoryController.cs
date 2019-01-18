24b2bf24bfbcdb0004414c8b13088f48ec9cff7c
Rohan Shahane <shahane.rohan96@gmail.com>
2019-01-18 17:58:16 +0530

    Added new API GetCategory to return list of categories

---
+++ Controllers/GetCategoryController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using BookStoreAPI.Models;
+using BookStoreAPI.CRUD;
+namespace BookStoreAPI.Controllers
+{
+    public class GetCategoryController : ApiController
+    {
+        // GET: api/GetCategory
+        public List<CategoryModel> Get()
+        {
+            GetCategoryLogic obj = new GetCategoryLogic();
+            return obj.GetCategory();
+
+        }
+
+        // GET: api/GetCategory/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/GetCategory
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/GetCategory/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/GetCategory/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Models/CategoryModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookStoreAPI.Models
+{
+    public class CategoryModel
+    {
+        public int ID { get; set; }
+        public string Category { get; set; }
+    }
+}
