5ee5b50d1ad9ddb0bc6d8a728b26fa76530c4952
Rohan Shahane <shahane.rohan96@gmail.com>
2019-02-23 12:42:38 +0530

    Updated CartSave API
    The bug which was causing API to fail after deleting orderline resolved.

--- CRUD/SaveCartLogic.cs
+++ CRUD/SaveCartLogic.cs
@@ -9,55 +9,111 @@ namespace BookStoreAPI.CRUD
 {
     public class SaveCartLogic
     {
+        // Logic to delete current orderlines and update BookTable quantity
         public void SaveCartFunction(List<SaveCartModel> orderlineList)
         {
-
-
-
             var con = @"Data Source=IL013449;Initial Catalog=Test;User ID=sa;Password=~$ystem32";       //Connection String
             SqlConnection myConnection = new SqlConnection(con);
-            SqlCommand deleteCommand = new SqlCommand("delete from OrderLines where UserID = " + orderlineList[0].UserID +"", myConnection);
             myConnection.Open();
-            deleteCommand.ExecuteNonQuery();
 
-            for(int i=0; i<orderlineList.Count;i++)
+            SqlCommand getTotalCountOfOrderLines = new SqlCommand("select count(*) from OrderLines where UserID = " + orderlineList[0].UserID.ToString() + "", myConnection);
+            int countOfOrderlines = Convert.ToInt32(getTotalCountOfOrderLines.ExecuteScalar());
+
+            List<int> listOfProductsInOldCart = new List<int>();
+
+            SqlCommand getProductIDsFromCurOrdLine = new SqlCommand("select ProductID from OrderLines where UserID = " + orderlineList[0].UserID, myConnection);
+            using (SqlDataReader myReader = getProductIDsFromCurOrdLine.ExecuteReader())
             {
-                //SqlCommand insertCommand = new SqlCommand("insert into OrderLines(OrderID,UserID,ProductID,Quantity) values(1,2,3,4)");
-                SqlCommand insertCommand = new SqlCommand("insert into OrderLines(OrderID,UserID,ProductID,Quantity) values(" + orderlineList[i].OrderID.ToString() + "," + orderlineList[i].UserID.ToString() + "," + orderlineList[i].ProductID.ToString() + "," + orderlineList[i].Quantity.ToString()  + ");", myConnection);
-                insertCommand.ExecuteNonQuery();
+                if (myReader.HasRows)
+                {
+                    while (myReader.Read())
+                    {
+                        int temp = myReader.GetInt32(myReader.GetOrdinal("ProductID"));
+                        listOfProductsInOldCart.Add(temp);
+                    }
+                }
             }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-            SqlDataReader myreader;
-            
-            
-            myreader = deleteCommand.ExecuteReader();
-            if (myreader.HasRows)
+            for (int i = 0; i < listOfProductsInOldCart.Count; i++)
             {
-                while (myreader.Read())
+                try
                 {
-                    l1.Add(new FetchBooksInfoModel() { ID = Convert.ToInt32(myreader[0]), Name = myreader[1].ToString(), Author = myreader[2].ToString(), Description = myreader[3].ToString(), Price = Convert.ToSingle(myreader[4]), Quantity = Convert.ToInt32(myreader[5]), CategoryID = Convert.ToInt32(myreader[6]), IconURL = myreader[7].ToString() });
+                    SqlCommand fetchCurBookQuantTable = new SqlCommand("select Quantity from BookTable where ID = " + listOfProductsInOldCart[i] + "", myConnection);
+                    int currQuantityInTable = (int)fetchCurBookQuantTable.ExecuteScalar();
+                    SqlCommand fetchOldOrdrelineProdQuant = new SqlCommand("select Quantity from OrderLines where ProductID = " + listOfProductsInOldCart[i].ToString() + " " + " and UserID = " + orderlineList[0].UserID.ToString() + "", myConnection);
+                    int oldOrderlineProdQuant = Convert.ToInt32(fetchOldOrdrelineProdQuant.ExecuteScalar());
+                    int finalQuant = currQuantityInTable + oldOrderlineProdQuant;
+                    SqlCommand updateBookQuant = new SqlCommand("update BookTable set Quantity = " + finalQuant.ToString() + " where ID = " + listOfProductsInOldCart[i] + "", myConnection);
+                    SqlCommand deleteCommand = new SqlCommand("delete from OrderLines where UserID = " + orderlineList[0].UserID + " and ProductID = " + listOfProductsInOldCart[i] +"", myConnection);
+                    deleteCommand.ExecuteNonQuery();
+                    updateBookQuant.ExecuteNonQuery();
                 }
-            }*/
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                }
+            }
+            // Logic to delete orderlines and update BookTable complete
+
+
+
+
+            
+                /* Apparantly redundant code
+                 * 
+                 * for (int i = 0; i < countOfOrderlines; i++)
+                {
+                    try
+                    {
+                        SqlCommand fetchCurBookQuantTable = new SqlCommand("select Quantity from BookTable where ID = " + orderlineList[i].ProductID + "", myConnection);
+                        int currQuantityInTable = (int)fetchCurBookQuantTable.ExecuteScalar();
+                        SqlCommand fetchOldOrdrelineProdQuant = new SqlCommand("select Quantity from OrderLines where ProductID = " + orderlineList[i].ProductID.ToString() + " " + " and UserID = " + orderlineList[i].UserID.ToString() + "", myConnection);
+                        int oldOrderlineProdQuant = Convert.ToInt32(fetchOldOrdrelineProdQuant.ExecuteScalar());
+
+
+                        int finalQuant = currQuantityInTable - oldOrderlineProdQuant;
+                        SqlCommand updateBookQuant = new SqlCommand("update BookTable set Quantity = " + finalQuant.ToString() + " where ID = " + orderlineList[i].ProductID.ToString() + "", myConnection);
+
+                        updateBookQuant.ExecuteNonQuery();
+                    }
+                    catch(Exception e1)
+                    {
+                        Console.WriteLine(e1);
+                    }
+                }*/
+
+            /*
+
+        for (int i = 0; i < orderlineList.Count; i++)
+        {
+            SqlCommand fetchCurBookQuantTable = new SqlCommand("select Quantity from BookTable where ID = " + orderlineList[i].ProductID + "", myConnection);
+            int currQuantityInTable = (int)fetchCurBookQuantTable.ExecuteScalar();
+            SqlCommand fetchOldOrdrelineProdQuant = new SqlCommand("select Quantity from OrderLines where ProductID = " + orderlineList[i].ProductID.ToString() + " " + " and UserID = " + orderlineList[i].UserID.ToString() + "",myConnection);
+            int oldOrderlineProdQuant = Convert.ToInt32(fetchOldOrdrelineProdQuant.ExecuteScalar());
+
+
+            int finalQuant = currQuantityInTable + oldOrderlineProdQuant;
+            SqlCommand updateBookQuant = new SqlCommand("update BookTable set Quantity = " + finalQuant.ToString() + " where ID = " + orderlineList[i].ProductID.ToString() + "", myConnection);
+
+            updateBookQuant.ExecuteNonQuery();
+        }*/
+
+
+
+            
+
+            for (int i = 0; i < orderlineList.Count; i++)
+            {
+                SqlCommand fetchCurBookQuantTable = new SqlCommand("select Quantity from BookTable where ID = " + orderlineList[i].ProductID + "", myConnection);
+                int currQuantityInTable = (int)fetchCurBookQuantTable.ExecuteScalar();
+                int finalQuant = currQuantityInTable - Convert.ToInt32(orderlineList[i].Quantity);
+                SqlCommand updateBookQuant = new SqlCommand("update BookTable set Quantity = " + finalQuant.ToString() + " where ID = " + orderlineList[i].ProductID.ToString() + "", myConnection);
+
+                SqlCommand insertCommand = new SqlCommand("insert into OrderLines(OrderID,UserID,ProductID,Quantity) values(" + orderlineList[i].OrderID.ToString() + "," + orderlineList[i].UserID.ToString() + "," + orderlineList[i].ProductID.ToString() + "," + orderlineList[i].Quantity.ToString() + ");", myConnection);
+                updateBookQuant.ExecuteNonQuery();
+                insertCommand.ExecuteNonQuery();
+            }
         }
     }
 }
